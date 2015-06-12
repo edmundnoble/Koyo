f282e775d9cab13bfe79cc3918cf0591e5ee09b4
Edmund Noble <eanoble@uwaterloo.ca>
2015-06-11 21:41:29 -0400

    Added scalas.

---
+++ app/src/main/scala/io/evolutionary/koyo/LoginActivity.scala
@@ -0,0 +1,37 @@
+package io.evolutionary.koyo
+
+;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+class LoginActivity extends ActionBarActivity {
+
+  protected override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_login, menu)
+    true
+  }
+
+  @Override
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    val id = item.getItemId
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings)
+      true
+    else
+      super.onOptionsItemSelected(item)
+  }
+}
