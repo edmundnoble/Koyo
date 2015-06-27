6098deb2044ef9c7a21e14b86294f3fd6f13d107
ItsPriyesh <priyesh.96@hotmail.com>
2015-06-27 02:07:02 -0400

    Add Navigation View

--- app/src/main/scala/io/evolutionary/koyo/ui/MainActivity.scala
+++ app/src/main/scala/io/evolutionary/koyo/ui/MainActivity.scala
@@ -1,27 +1,38 @@
 package io.evolutionary.koyo.ui
 
+import android.support.design.widget.NavigationView
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.view.MenuItem
 import io.evolutionary.koyo.JobSearch.SearchParams
 import io.evolutionary.koyo.{JobSearch, R}
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import io.evolutionary.koyo.R
->>>>>>> Stashed changes
 import io.evolutionary.koyo.ui.applications.ApplicationsFragment
 import io.evolutionary.koyo.ui.common.BaseActivity
 
-class MainActivity extends BaseActivity {
+class MainActivity extends BaseActivity with NavigationView.OnNavigationItemSelectedListener {
 
+  private lazy val applicationsFragment = new ApplicationsFragment
   private lazy val fragmentManager = getSupportFragmentManager
+  private lazy val navigationView: NavigationView = findView(R.id.navigation_view)
 
   protected override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
     injectToolbar()
     getSupportActionBar setTitle "Applications"
-    swapFragment(new ApplicationsFragment)
+    swapFragment applicationsFragment
+    navigationView setNavigationItemSelectedListener this
+  }
+
+  override def onNavigationItemSelected(menuItem: MenuItem): Boolean = {
+    swapFragment(menuItem.getItemId match {
+      case R.id.nav_applications => applicationsFragment
+    })
+    true
   }
 
   private def swapFragment(fragment: Fragment): Unit =
